3df1f988d8e9690f5ace1e70eece36a60c1406e6
Anya-Desdein <heavelyn1999@gmail.com>
2023-01-05 23:22:01 +0100

    Day 1 part 2 refactoring

--- 01/index.js
+++ 01/index.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 let data = fs.readFileSync("./input.txt", { encoding: "utf-8" })
     .split("\n\n")
-    .map( x =>  x.split("\n")
+    .map( x =>  x
+        .split("\n")
         .reduce((acc, val) =>  acc + parseInt(val), 0)
     )
     .reduce((acc, val) => val > acc ? val : acc, 0);
     
-
 console.log(data);
--- 01/index2.js
+++ 01/index2.js
@@ -1,34 +1,11 @@
 const fs = require('fs');
 
-// function sortFn(a, b) {
-//     const ret = a - b;
-//     console.log("Sorting:", a, b, "->", ret);
-//     return ret;
-// }
-
-// const numbers = [3, 1, 4, 1, 5];
-
-// const sorted = numbers.sort(sortFn);
-// // numbers and sorted are both [1, 1, 3, 4, 5]
-// // sorted[0] = 10;
-// console.log(numbers[0]); // 10
-// console.log(numbers[0]+ numbers[1] + numbers[2]);
-
-
-
-
-const inputLines = 
-  fs.readFileSync("./input.txt", { encoding: "utf-8" })
-    .replace(/\r\n/g, "\n")
+let data = fs.readFileSync("./input.txt", { encoding: "utf-8" })
     .split("\n\n")
-    .map(s => s
+    .map( x =>  x
         .split("\n")
-        .map(parseFloat)
-        .filter(x => !isNaN(x))
-        .reduce((prev, curr) => curr + prev)
-    );
+        .reduce((acc, val) =>  acc + parseInt(val), 0)
+    )
+    .sort((a, b) => b - a);
 
-inputLines.sort((a, b) => b - a)
-
-console.log(inputLines[0]);
-console.log(inputLines[0]+ inputLines[1] + inputLines[2]);
+console.log(data[0] + data[1] + data[2]);
